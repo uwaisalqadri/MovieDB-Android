a6a35275455a9246759f1bdbe7e93d8417b08d75
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-04-05 19:25:39 +0800

    creating the ui

--- app/src/main/java/com/uwaisalqadri/muvi_app/ui/MainActivity.kt
+++ app/src/main/java/com/uwaisalqadri/muvi_app/ui/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.uwaisalqadri.muvi_app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.uwaisalqadri.muvi_app.R
+import com.uwaisalqadri.muvi_app.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+	private val binding: ActivityMainBinding by viewBinding()
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		binding.apply {
+			navView.apply {
+				setupWithNavController(findNavController(R.id.nav_host_fragment))
+				itemIconTintList = null
+			}
+		}
 	}
 }
